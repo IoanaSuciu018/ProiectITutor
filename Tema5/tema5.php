2b9d3a1b207d9d479867a90b9e1b74bbb14f9271
IoanaSuciu018 <ioana.suciu018@yahoo.com>
2020-02-09 15:41:40 +0200

    Tema5

--- Tema5/tema5.php
+++ Tema5/tema5.php
@@ -1,13 +1,13 @@
 <?php
 // Greeting created with echo statement
-echo "Hello class!<br>";
-echo "I'm about to learn PHP!<br>";
+echo "Hello class!<br/>";
+echo "I'm about to learn PHP!<br/>";
 echo "This ", "is ", "way ", "to hard ", "for me.";
 // Data types (question : why x is definded with double " and y with only one', this is applyed per general?)
 $x = "Ioana";
 $y = 'Suciu';
 echo $x;
-echo "<br>"; 
+echo "<br/>"; 
 echo $y;
 // Strings - all this are easy
 echo str_word_count("Care este asemanarea dintre un Windows si un submarin?");
