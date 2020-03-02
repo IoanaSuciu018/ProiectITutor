e47a026dc2c4dfbf334289a0858ff9223801c2eb
IoanaSuciu018 <ioana.suciu018@yahoo.com>
2020-03-02 20:18:10 +0200

    Intrebari+SQL

---
+++ Tema8/SQL Users.sql
@@ -0,0 +1,31 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.10-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for proiect
+CREATE DATABASE IF NOT EXISTS `proiect` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `proiect`;
+
+-- Dumping structure for table proiect.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `Nume` varchar(255) NOT NULL,
+  `Prenume` varchar(255) NOT NULL,
+  `Telefon` varchar(30) NOT NULL,
+  `Adresa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
